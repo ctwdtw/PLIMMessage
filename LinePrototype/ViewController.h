38ac1403da0ad2d86b7d3cc70999b3f50c24f85f
Paul Lee <ctwdtw@gmail.com>
2015-08-19 10:34:05 +0800

    Initial Commit

---
+++ LinePrototype/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LinePrototype
+//
+//  Created by Paul Lee on 2015/8/19.
+//  Copyright (c) 2015年 Paul Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LinePrototype/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LinePrototype
+//
+//  Created by Paul Lee on 2015/8/19.
+//  Copyright (c) 2015年 Paul Lee. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
